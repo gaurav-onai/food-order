c3444cae6fa6751a9a32005c6fd45a8523ec2b72
Gaurav.Raj <gaurav.raj@oaknorth.ai>
2020-06-01 01:11:56 +0530

    added mc

---
+++ src/app/restaurant-list/restaurant-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'food-order-restaurant-list',
+  templateUrl: './restaurant-list.component.html',
+  styleUrls: ['./restaurant-list.component.scss']
+})
+export class RestaurantListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
